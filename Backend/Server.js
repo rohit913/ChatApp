ea6bab8aac55dcb6055f86708af72516d1624759
Rohit sharma <rsharma07feb@gmail.com>
2024-02-26 16:18:21 +0530

    2nd part ciompleted  - ui design

--- Backend/Server.js
+++ Backend/Server.js
@@ -23,11 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.send(" rohit sharma");
-  // res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  // res.send(" rohit sharma");
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
